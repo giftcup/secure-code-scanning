e7d528e6b1a0a0de7d8e61b712dc82cb5c64aa83
giftcup <salometambe@gmail.com>
2023-11-24 18:39:30 +0100

    Make use of Google OSV API to scan for vulnerabilities in package.json files

--- get_vul_packages.py
+++ get_vul_packages.py
@@ -1,5 +1,5 @@
 import json
-# import requests
+import requests
 
 def read_json_file (json_file_path) :
     with open(json_file_path, 'r') as file:
@@ -13,7 +13,52 @@ def get_dependencies(package_json) :
     print(all_dependencies)
     return all_dependencies
 
+def get_vulnerabilities(package_name, package_version, package_ecosystem="npm") :
+    headers = {"Content-Type": "application/json"}
+    url = "https://api.osv.dev/v1/query"
+    query = {
+        "version": package_version, 
+        "package": {
+            "name": package_name,
+            "ecosystem": package_ecosystem
+        }
+    }
 
-json_file_path = input("Enter path to package.json file: ")
-package_json = read_json_file(json_file_path)
-dependencies = get_dependencies(package_json)
+    response = requests.post(url, data = json.dumps(query), headers = headers)
+
+    if response.status_code == 200:
+        vulnerabilities = response.json()
+        return vulnerabilities
+    else:
+        print(f"Failed to return vulnerabilities for {package_name}@{package_version} for the {package_ecosystem} ecosystem")
+        return None
+    
+
+def main() :
+    json_file_path = input("Enter path to package.json file: ")
+
+    package_json = read_json_file(json_file_path)
+    dependencies = get_dependencies(package_json)
+    
+    vulnerability_dictionary = {}
+
+    for package_name, package_version in dependencies.items():
+        vulnerabilities = get_vulnerabilities(package_name, package_version)
+
+        if vulnerabilities:
+            print(f"Vulnerabilities found for {package_name}@{package_version}:")
+
+            for vulnerability in vulnerabilities['vulns']:
+                vulnerability_id = vulnerability['id']
+                vulnerability_summary = vulnerability['summary']
+
+                vulnerability_dictionary[vulnerability_id] = vulnerability_summary
+                print("💣------------------------------------")
+                print(f"\tID: {vulnerability_id}")
+                print(f"\tSummary: {vulnerability_summary}")
+                print("💣------------------------------------")
+            print(f"{vulnerability_dictionary}")
+    
+    if not vulnerability_dictionary:
+        print("\nHurray🎉🎉, you're package.json is free of vulnerabilities💃💃\n")              
+main()
